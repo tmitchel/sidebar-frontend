228ddb893706752ac0f87d57b09a435034ac8ace
Tyler Mitchell <tyler106@ksu.edu>
2020-05-11 18:58:58 -0500

    remove joke

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,10 +1,7 @@
 <template>
   <v-container>
-    <!-- <h1>Help</h1> -->
-    <!-- <p>Just type stuff or whatever, ok?</p> -->
-    <h1>Sidebar</h1>
-    <h3>For when it's time to stop dicking around and get some fucking shit done.</h3>
-    <img src="img.png" alt="" height="500">
+    <h1>Help</h1>
+    <p>Just type stuff or whatever, ok?</p>
   </v-container>
 </template>
 
